166ad047ca338411f6eaffdff6d12f4f5aae4bc9
Alejandro J Cisnero A <114025066+alecisnero@users.noreply.github.com>
2024-04-23 15:50:27 +0000

    final del proyec

--- src/models.py
+++ src/models.py
@@ -7,26 +7,40 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True, nullable=False)
+    user_from_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user_to_id = Column(Integer, ForeignKey('user.id'), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True, nullable=False) 
+    usarname = Column(String(250), nullable=False)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
-    def to_dict(self):
-        return {}
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True,nullable=False)
+    post_id = Column(Integer, ForeignKey('user.id'),nullable=False)
+
+class Media(Base):
+    __tablename__ = 'media'
+    id =Column(Integer, primary_key=True, nullable=False) 
+    type =Column(String(250), nullable=True)
+    url =Column(String(250), nullable=False) 
+    post_id =Column(Integer, ForeignKey('post.id'), nullable=False)
+
+class Commets(Base):
+    __tablename__ = 'commets'
+    id = Column(Integer, primary_key=True, nullable=False)
+    author_id = Column(String(250), ForeignKey('user.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+    text = Column(Integer, nullable=False)
+
+
 
 ## Draw from SQLAlchemy base
 try:
